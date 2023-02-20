bd5de4f9584d37badd31f89bb6ae8cc893830fc7
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-02-20 14:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
-
+var btn= document.getElementById("btn");
+var numPtag = document.getElementById("num");
+var res = document.getElementById("respond");
+var inputValue = document.getElementById("guess").value;
+function randomNum() {
+	no = Math.floor(Math.random()*40 -20);
+	document.getElementById("num").innerText = no;
+	guessNum();
+	print();
+}
+function guessNum() {
+	input = Number(document.getElementById("guess").value);
+}
+function print() {
+	if(Math.abs(no-input) <=5){
+		document.getElementById("respond").innerText="Hot";
+	}
+	else{
+		document.getElementById("respond").innerText="Cold";
+	}
+}
+btn.addEventListener("click" , randomNum);
